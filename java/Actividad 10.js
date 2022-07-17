13b4ff87fbc27b5456626f9ef4a8862eb07dd77e
Martina Baz <109114046+ShiroMeiko@users.noreply.github.com>
2022-07-17 18:54:29 -0300

    Add files via upload

---
+++ java/Actividad 10.js
@@ -0,0 +1,40 @@
+let nombre = "Programación" + " Fácil.";
+
+let saludo = "Les doy la bienvenida a ";
+
+let fraseCompleta = saludo + nombre;
+
+console.log(fraseCompleta);
+
+let frase = "JavaScript " + "es un lenguaje" + "esencial para crear" + " sitios web poderosos. ";
+
+console.log(frase);
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+console.log(concatenaNumeros);
+
+
+sexo = prompt("introduce tu sexo (femenino o masculino)");
+
+if (sexo = "femenino") {
+
+    edad = prompt("Introduce tu edad");
+    if (edad >= 20) {
+        alert("Puedes entrar, eres mayor de edad.");
+    } else {
+        alert("No puedes entrar, eres menor de edad.");
+    }
+} else if (sexo = "masculino") {
+
+    edad = prompt("Introduce tu edad");
+    if (edad >= 20) {
+        alert("Puedes entrar, eres mayor de edad.");
+    } else {
+        alert("No puedes entrar, eres menor de edad.");
+    }
+} else {
+    alert("no has introducido tu sexo correctamente")
+}
